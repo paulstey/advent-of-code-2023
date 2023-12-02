502ca2d9574b727649903a5572aaf85cc581fc47
Paul Stey <paulstey@gmail.com>
2023-12-02 14:12:39 -0500

    feat: solve day1 part2

--- day1_trebuchet/src/main.rs
+++ day1_trebuchet/src/main.rs
@@ -1,44 +1,11 @@
+use anyhow::Result;
+use std::collections::HashMap;
 use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
 };
-
 use time::Instant;
-
-#[derive(Debug)]
-struct Calibration {
-    first: i32,
-    last: i32,
-    value: i32,
-}
-
-impl Calibration {
-    fn from(input: &str) -> Self {
-        let mut numbers = extract_numbers(input);
-
-        let first = numbers
-            .chars()
-            .nth(0)
-            .unwrap()
-            .to_digit(10)
-            .unwrap()
-            .try_into()
-            .unwrap();
-
-        let last = numbers
-            .pop()
-            .expect("`numbers` string is empty")
-            .to_digit(10)
-            .unwrap()
-            .try_into()
-            .unwrap();
-
-        let value = first * 10 + last;
-
-        Self { first, last, value }
-    }
-}
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("No such file...");
@@ -48,16 +15,85 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn extract_numbers(input: &str) -> String {
-    let mut numbers = String::from("");
+fn get_first_number(
+    input: &String,
+    num_words_hmap: &HashMap<&str, i32>,
+    digits: &Vec<&str>,
+) -> Result<i32> {
+    let mut first_idx = 999;
+    let mut number = 999;
 
-    input.chars().for_each(|c| {
-        if c.is_ascii_digit() {
-            numbers.push(c)
+    for (word, num) in num_words_hmap {
+        match input.find(word) {
+            Some(idx) => {
+                if idx < first_idx {
+                    first_idx = idx;
+                    number = *num;
+                }
+            }
+            None => continue,
         }
-    });
+    }
 
-    numbers
+    for num in digits {
+        match input.find(num) {
+            Some(idx) => {
+                if idx < first_idx {
+                    first_idx = idx;
+                    number = num.parse::<i32>()?;
+                }
+            }
+            None => continue,
+        }
+    }
+
+    Ok(number)
+}
+
+fn get_last_number(
+    input: &String,
+    num_words_hmap: &HashMap<&str, i32>,
+    digits: &Vec<&str>,
+) -> Result<i32> {
+    let mut last_idx = 0;
+    let mut number = 999;
+
+    for (word, num) in num_words_hmap {
+        match input.rfind(word) {
+            Some(idx) => {
+                if idx >= last_idx {
+                    last_idx = idx;
+                    number = *num;
+                }
+            }
+            None => continue,
+        }
+    }
+
+    for num in digits {
+        match input.rfind(num) {
+            Some(idx) => {
+                if idx >= last_idx {
+                    last_idx = idx;
+                    number = num.parse::<i32>()?;
+                }
+            }
+            None => continue,
+        }
+    }
+
+    Ok(number)
+}
+
+fn get_calibration_value(
+    input: &String,
+    num_words_hmap: &HashMap<&str, i32>,
+    digits: &Vec<&str>,
+) -> Result<i32> {
+    let first = get_first_number(input, num_words_hmap, digits)?;
+    let last = get_last_number(input, num_words_hmap, digits)?;
+
+    Ok(first * 10 + last)
 }
 
 fn main() {
@@ -65,11 +101,28 @@ fn main() {
 
     let input = lines_from_file("./data/day1.txt");
 
+    let num_words_hmap = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let digits = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
     let mut soln = 0;
 
-    let calibs_vec: Vec<_> = input.iter().map(|line| Calibration::from(&line)).collect();
+    let calibs_vec: Vec<_> = input
+        .iter()
+        .map(|line| get_calibration_value(line, &num_words_hmap, &digits).unwrap_or(0))
+        .collect();
 
-    calibs_vec.iter().for_each(|calib| soln += calib.value);
+    calibs_vec.iter().for_each(|value| soln += value);
 
     println!("{:?}", Instant::now() - t1);
 
