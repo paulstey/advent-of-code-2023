14a3ef90077a8a2953d4e404dc5e5d015833fa46
Paul Stey <paulstey@gmail.com>
2023-12-01 20:38:59 -0500

    feat: solve day1 part 1

--- day1_trebuchet/src/main.rs
+++ day1_trebuchet/src/main.rs
@@ -1,16 +1,51 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+use time::Instant;
+
+#[derive(Debug)]
 struct Calibration {
     first: i32,
     last: i32,
+    value: i32,
 }
 
 impl Calibration {
     fn from(input: &str) -> Self {
-        let numbers = extract_numbers(input);
-        let first = numbers.chars()[0].parse::<i32>();
-        let last = numbers.pop().parse::<i32>();
+        let mut numbers = extract_numbers(input);
 
-        Self { first, last }
+        let first = numbers
+            .chars()
+            .nth(0)
+            .unwrap()
+            .to_digit(10)
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        let last = numbers
+            .pop()
+            .expect("`numbers` string is empty")
+            .to_digit(10)
+            .unwrap()
+            .try_into()
+            .unwrap();
+
+        let value = first * 10 + last;
+
+        Self { first, last, value }
     }
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("No such file...");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line..."))
+        .collect()
 }
 
 fn extract_numbers(input: &str) -> String {
@@ -26,10 +61,17 @@ fn extract_numbers(input: &str) -> String {
 }
 
 fn main() {
-    let input = "123 abc 456 def 789";
-    let numbers = extract_numbers(input);
+    let t1 = Instant::now();
 
-    let calib = Calibration::from(input);
+    let input = lines_from_file("./data/day1.txt");
 
-    println!("{:?}", calib);
+    let mut soln = 0;
+
+    let calibs_vec: Vec<_> = input.iter().map(|line| Calibration::from(&line)).collect();
+
+    calibs_vec.iter().for_each(|calib| soln += calib.value);
+
+    println!("{:?}", Instant::now() - t1);
+
+    println!("{:?}", soln);
 }
